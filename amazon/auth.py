de727b226015d791eef0e30fb5c961e6ad1be821
rroach29 <148112831+rroach29@users.noreply.github.com>
2026-06-28 17:34:39 -0600

    Update auth.py

--- amazon/auth.py
+++ amazon/auth.py
@@ -1,1 +1,13 @@
 
+from amazon_ads import get_access_token
+
+
+def get_headers():
+    token = get_access_token()
+
+    return {
+        "Authorization": f"Bearer {token}",
+        "Amazon-Advertising-API-ClientId": __import__("os").environ["AMAZON_CLIENT_ID"],
+        "Amazon-Advertising-API-Scope": __import__("os").environ["AMAZON_PROFILE_ID"],
+        "Content-Type": "application/json",
+    }
